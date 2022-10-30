595260cdcb3844ffecfa41490a6189fc196d560b
Syntheticz <guiangphilip08@gmail.com>
2022-10-30 22:14:08 +0800

    added file handling

--- main.cpp
+++ main.cpp
@@ -7,13 +7,14 @@
 #include <iomanip>
 #include <sstream>
 
+#include "hash/sha256.h"
 
 using namespace std;
 
 extern "C" char* strptime(const char* str, const char* format, struct tm* time) {
-  std::istringstream input(str);
-  input.imbue(std::locale(setlocale(LC_ALL, nullptr)));
-  input >> std::get_time(time, format);
+  istringstream input(str);
+  input.imbue(locale(setlocale(LC_ALL, nullptr)));
+  input >> get_time(time, format);
   if (input.fail()) {
     return nullptr;
   }
@@ -25,7 +26,7 @@ extern "C" char* strptime(const char* str, const char* format, struct tm* time) 
 
 /// expression
 
-regex numberEx("[-+]?([0-9]*\.[0-9]+|[0-9]+)");
+regex numberEx("[-+]?([0-9]*\\.[0-9]+|[0-9]+)");
 regex nameEx("^[a-zA-Z\\s]*$");
 
 // Data structure for Recipts
@@ -63,9 +64,9 @@ string get_uuid() {
 // Data structure for Users
 
 typedef struct info{
-    string name, birthDay, contact;
-    int accountNumber, pincode;
-    float deposit;
+    string name, birthDay, contact, pincode;
+    int accountNumber;
+    float savings;
 
 }INFO;
 
@@ -81,22 +82,37 @@ typedef struct userRecords{
 class User {
     private:
     UREC *head;
+    UREC *acc;
 
     public:
     void setHead();
     void menu();
     void openAcc();
     void registerAcc();
-    void add(INFO inf);
+    void add(INFO inf, UREC *point);
+    void save();
+    void retrieve(string path, int mode);
     bool validate(int mode, string value);
+    
+    //for Oppended Accounts
+    bool checkPin();
+    void checkBal();
+    void withdraw();
+    void deposit(int amount);
+    void fundTransfer(int sent, int recieved);      
+    void changePin();
+    void accountMenu();
+    // TODO Add features.    
 };
 
 void User::setHead(){
     head = NULL;
+    acc = NULL;
 }
-void User::add(INFO inf){
+
+void User::add(INFO inf, UREC *point){
     UREC *pointer, *follower, *temp;
-    pointer = follower = head;
+    pointer = follower = point;
 
     temp = new UREC;
     temp->inf = inf; 
@@ -107,8 +123,8 @@ void User::add(INFO inf){
         pointer = pointer->next;
     }
 
-    if(head == pointer){
-        head = temp;
+    if(point == pointer){
+        point = temp;
     }else{
         follower->next = temp;
         temp->next = pointer;
@@ -210,13 +226,13 @@ void User::registerAcc(){
     cout << "Please re-enter your pincode: ";
     getline(cin, buffer);
     if(buffer != pincode){cout << "The pincode that you've entered does not match" << endl; return;}
-    usr.pincode = stoi(pincode);
+    usr.pincode = sha256(pincode);
 
     cout << "Please enter initial deposit: ";
     getline(cin, buffer);
     if(validate(4, buffer)){return;}
     if(stoi(buffer) < 5000){cout << "initial deposit must be a minimum of 5000!" << endl; return;}
-    usr.deposit = stoi(buffer);
+    usr.savings = stoi(buffer);
 
     // confirmation notice
     cout << "\e[1;1H\e[2J" << endl;
@@ -224,19 +240,168 @@ void User::registerAcc(){
     cout << "Name: " << usr.name << endl;
     cout << "Number: " << usr.contact << endl;
     cout << "birthday: " << usr.birthDay << endl;
-    cout << "initial Deposit: " << usr.deposit << endl;
+    cout << "initial Deposit: " << usr.savings << endl;
     cout << "\n\nType [Y] if all of the information are correct.\nType [N] if you want to re-enter our information: ";
     getline(cin, choice);
-    if(choice == "Y" || choice == "y"){buffer = get_uuid(); cout << "Your uniqe id is: " << buffer << endl; usr.accountNumber = stoi(buffer);system("pause"); menu();}
+    if(choice == "Y" || choice == "y"){buffer = get_uuid(); cout << "Your uniqe id is: " << buffer << endl; usr.accountNumber = stoi(buffer);system("pause"); 
+    add(usr, head); menu();}
 
-    add(usr);
 
 }
 
 void User::openAcc(){
-    cout << "Open Account" << endl;
+    cout << "\e[1;1H\e[2J" << endl;
+    fstream fp;
+    do
+    {
+        cout << "\e[1;1H\e[2J" << endl;
+        cout << "Please insert your card!" << endl;
+        fp.open("record.txt", ios::in);
+    } while (!fp);
+    fp.close();
+    cout << "\e[1;1H\e[2J" << endl;
+    cout << "Card detected..." << endl;
+    retrieve("record.txt", 0);
+    system("pause");
+    
+    accountMenu();
 }
 
+void User::save(){
+    fstream fp;
+    UREC *pointer = head;
+    fp.open("records.txt", ios::out);
+    if(!fp){
+        cout<<" Error while creating the file ";     
+    }else{  
+        while(pointer != NULL){
+            fp << pointer->inf.name << "\n" << pointer->inf.accountNumber << " " << pointer->inf.pincode << " " << pointer->inf.birthDay << " " << pointer->inf.savings << endl;
+            pointer = pointer->next;
+        }
+        fp.close();
+            }
+}
+
+//TODO Fix the point being NULL
+
+void User::retrieve(string path, int mode){
+    fstream fp;
+    INFO rec;
+    UREC *temp = mode ? head : acc;
+
+    fp.open(path, ios::in);
+    if(!fp){
+        cout<<" Error while creating the file "; 
+    }else{
+        if(fp.is_open()){
+            while(true){
+                getline(fp, rec.name, '\n');
+                fp >> rec.accountNumber >> rec.pincode >> rec.birthDay >> rec.savings;
+                fp.ignore();
+                if(!fp.eof()){
+                    add(rec, temp);
+                    cout << temp->inf.pincode << endl;
+                    system("pause");
+                }else{
+                    break;
+                }
+               
+
+            }
+            fp.close();
+
+        }
+    }
+}
+
+
+bool User::checkPin(){
+    string pin;
+    cout << "Please enter your PIN: ";
+    getline(cin, pin);
+    if(!(regex_match(pin, numberEx))){
+        cout << "Please enter a number!" << endl;
+        return false;
+    }
+
+    if(sha256(pin) == acc->inf.pincode){
+        return true;
+    }
+    return false;
+}
+
+void User::withdraw(){
+    string amount;
+    cout << "Remaining Balance: " << acc->inf.savings << endl;
+    cout << "Much do you want to withdraw [1-" << acc->inf.savings << "]: ";
+    getline(cin, amount);
+    if(!(regex_match(amount, numberEx))){
+        cout << "Please enter a number!" << endl;
+        withdraw();
+    }
+    if(stoi(amount) > acc->inf.savings){
+        cout << "Error: The amount you entered is more than your savings!" << endl;
+        system("pause");
+        withdraw();
+    }
+    acc->inf.savings = acc->inf.savings - stoi(amount);
+    cout << "\e[1;1H\e[2J" << endl;
+    cout << "Withdrawn successfully..." << endl;
+    system("pause");
+}
+
+void User::checkBal(){
+    cout << "Current balance: " << acc->inf.savings << endl;
+}
+
+
+void User::accountMenu(){
+    cout << "\e[1;1H\e[2J" << endl;
+    cout << "Welcome to Student Banks Inc." << endl;
+    cout << "[1] Balance Inquiry" << endl;
+    cout << "[2] Withdraw" << endl;
+    cout << "[3] Deposit" << endl;
+    cout << "[4] Fund Transfer" << endl;
+    cout << "[5] Change PIN code" << endl;
+    cout << "[6] More options..." << endl;
+    cout << "[7] Exit" << endl;
+
+    string c;
+    getline(cin, c);
+     if(!(regex_match(c, numberEx))){
+        cout << "Please enter a number!" << endl;
+        return;
+    }
+    
+
+    switch (stoi(c))
+    {
+    case 1:
+        checkBal();
+        break;
+    case 2:
+        withdraw();
+        break;
+    case 3:
+        /* code */
+        break;
+    case 4:
+        /* code */
+        break;
+    case 5:
+        /* code */
+        break;
+    case 6:
+        /* code */
+        break;
+    case 7:
+        exit(0);
+        break;
+    default:
+        break;
+    }
+
+}
 
 
 void User::menu(){
@@ -270,6 +435,7 @@ void User::menu(){
         openAcc();
         break;
     case 3:
+        save();
         exit(0);
         break;
     default:
@@ -282,11 +448,14 @@ void User::menu(){
 
 
 int main(){
-
-    User *us = new User;
+    User us;
+    us.setHead();
+    us.retrieve("records.txt", 1);
     while (1)
     {
-        us->menu();
+        us.menu();
     }
+
+    system("cd crypt && ccrypt -e ../record.txt -K 1234");
 
 }
