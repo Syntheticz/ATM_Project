67b4bbe2bdd1e4d1f72cb946a8e51ab5f17699d8
Syntheticz <guiangphilip08@gmail.com>
2022-11-01 15:33:45 +0800

    added somethings

--- header/functions.h
+++ header/functions.h
@@ -7,4 +7,16 @@
 #include <iomanip>
 #include <sstream>
 
+#include "filehandling.h"
+#include "display.h"
+
+
 using namespace std;
+
+class Functions{
+    protected:
+        UREC *head;
+        UREC *acc;
+    public:
+    void setHead();
+};
